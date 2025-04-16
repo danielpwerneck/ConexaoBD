03576fc5eb1f141dda8cfd4f7d4e228529749568
Daniel do Prado Werneck <danielpwerneck@gmail.com>
2025-04-16 20:04:37 -0300

    projeto base

---
+++ Conexao.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+
+namespace ConexaoBD
+{
+        class Conexao
+    {
+
+        string dadosConexao ="server=localhost;user=root;database=banco_ti41;port=3306;password=";
+
+        public List<Produto> BuscaProdutos()
+        {
+            //Abrir conexão com banco
+            MySqlConnection conexao = new MySqlConnection(dadosConexao);
+            conexao.Open();
+            Console.WriteLine("Conexão realizada com sucesso");
+
+            //Rodar o SQL dentro do banco
+            string sql= "SELECT * FROM produtos;";
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+            MySqlDataReader dados = comando.ExecuteReader();
+
+            List<Produto> lista = new List<Produto>();
+
+            while (dados.Read())
+            {
+                //Console.WriteLine("ID:"+dados[0]+"| Nome: "+dados[1]+" | Preço:"+dados[2]);
+
+                Produto p = new Produto();
+                p.id = dados.GetInt32("id");
+                p.nome = dados.GetString("nome");
+                p.preco = dados.GetFloat("preco");
+                p.registro = dados.GetDateTime("registro");
+                lista.Add(p);
+            }
+
+            conexao.Close();
+
+            return lista;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Org.BouncyCastle.Asn1.Misc;
+
+namespace ConexaoBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   
+
+            Conexao conexao = new Conexao();
+            List<Produto> lista = conexao.BuscaProdutos();
+
+            foreach( Produto p in lista){
+                Console.WriteLine("ID:"+p.id+"Nome" +p.nome);
+            }
+
+
+
+           // Console.WriteLine("Hello World!");
+           // Console.WriteLine("Olá, digite o seu nome:");
+            // string nome = Console.ReadLine();
+            // Console.WriteLine("Seja bem vinda Sre"+nome);
+
+
+            //Produto produto = new Produto();
+           // produto.nome = "Lençol";
+           // produto.preco = 78.99f;
+
+            //Console.WriteLine("O produto é: "+produto.nome+" e o preço é "+produto.preco);
+
+
+
+        }
+    }
+}
